7d84cc791843f1fcb0171ab35423443737cdfad4
Amanda Katt <akatt@umass.edu>
2022-04-20 20:52:37 -0400

    endpoints finalized

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/employer/homepage', (req, res) => {
 
 //route to applicants list for a specified job for employer to view
 //NOT DONE YET
-app.get('/jobs/:jobID/applicants', (req, res) => {
+app.get('/employer/:jobID/applicants', (req, res) => {
     res.sendFile(__dirname + '/static/applicants.html');
 });
 
@@ -66,18 +66,27 @@ var loginValidation = [
     check('password').isLength({ min: 8}).withMessage('password must be at least 8 characters').trim().escape()
 ];
 //process input
-app.post('/login', loginValidation, (req, res) => {
+app.post('/user/login', loginValidation, (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
     } else {
         let email = req.body.email;
         let password = req.body.password;
-        res.send(`Email: ${email} Password: ${password}`);
+        res.send(`Email: ${email} Password: ${password} Type: user`);
     }
 });
 
-
+app.post('/employer/login', loginValidation, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    } else {
+        let email = req.body.email;
+        let password = req.body.password;
+        res.send(`Email: ${email} Password: ${password}  Type: employer`);
+    }
+});
 
 
 //CREATE STUFF HERE:
@@ -114,7 +123,22 @@ app.get('/users', (req, res) => {
 
 //response will be JSON object representing all employers
 app.get('/employers', (req, res) => {
-    res.json(employers)
+    res.json(employers);
+});
+
+//response will be JSON object specified jobs 
+app.get('/employer/job/:jobID', (req, res) => {
+    res.json(jobs[req.jobID]);
+});
+
+//response will be JSOn object specified user
+app.get('/user/:email', (req, res) => {
+    res.json(users[req.email]);
+});
+
+//response will be JSON object representing specified employer
+app.get('/employer/:email', (req, res) => {
+    res.json(employers[req.email]);
 });
 
 
@@ -131,13 +155,17 @@ app.put('/user/:email/apply/:jobID', async (req, res) => {
 
 
 //DELETE STUFF HERE:
-
-app.delete('/job/delete/:jobID', async (req, res) => {
+app.delete('/employer/job/delete/:jobID', async (req, res) => {
     utilities.deleteJob(req.param.jobID);
 });
 
+app.delete('/employer/delete/:email', async (req, res) => {
+    utilities.deleteEmployer(res, req.email);
+});
 
-
+app.delete('/user/delete/:email', async (req, res) => {
+    utilities.deleteUser(res, req.email);
+});
 
 
 app.get('*', (req, res) => {
