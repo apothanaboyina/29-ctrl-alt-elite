6a5c98f47f9cc119729a457eadfed7ea3d86c137
apothanaboyina <apothanaboyi@umass.edu>
2022-04-20 22:15:52 -0400

    edits

--- static/applicants.js
+++ static/applicants.js
@@ -1,3 +1,6 @@
 //this is not the official url but just example for now
 const url = "https://ctrl-alt-elite-umass-job-board";
 
+goback.addEventListener("click", () => {
+    window.history.back();
+});
--- static/employerinterface.js
+++ static/employerinterface.js
@@ -1,3 +1,17 @@
 //this is not the official url but just example for now
 const url = "https://ctrl-alt-elite-umass-job-board";
 
+const view = document.getElementById("view");
+const edit = document.getElementById("edit");
+const post = document.getElementById("post");
+
+
+post.addEventListener("click", () => {
+    const title = document.getElementById("title");
+    const desc = document.getElementById("desc");
+    const rate = document.getElementById("rate");
+    const hours = document.getElementById("hours");
+    const location = document.getElementById("location");
+
+    
+});
