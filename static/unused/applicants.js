8d41f36f4587b6246c668e5be5d77eca44abfa18
Amanda Katt <akatt@umass.edu>
2022-05-07 20:34:49 -0400

    major database updates

--- static/unused/applicants.js
+++
@@ -1,6 +0,0 @@
-//this is not the official url but just example for now
-const url = "https://ctrl-alt-elite-umass-job-board";
-
-goback.addEventListener("click", () => {
-    window.history.back();
-});
